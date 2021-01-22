66c319a4a9bbb7e33bfe15e805b6e6ccd400cdb6
AimerNeige <aimer.neige.soft@gmail.com>
2021-01-22 17:55:32 +0800

    update alarm proj

---
+++ Alarm/main.c
@@ -0,0 +1,32 @@
+/**
+ * author: AimerNeige
+ * generation time: 2021/01/22
+ * filename: main.c
+ * language & build version : C 11
+*/
+#include <reg52.h>
+#include <intrins.h>
+
+#define ON  1
+#define OFF 0
+
+sbit fm = P2^3;
+
+void delay(void)   //误差 -0.000000000023us
+{
+    unsigned char a,b;
+    for(b=221;b>0;b--)
+        for(a=207;a>0;a--);
+}
+
+void main()
+{
+	P1 = 0xfe;
+	while (1) {
+		delay();
+		P1 = _crol_ (P1, 1);
+		fm = ON;
+		delay();
+		fm = OFF;
+	}
+}
